576d65d1fc36436e1d4ca96d470ce8517d9be187
Kirill <cirill@mail.ru>
2023-01-24 17:59:00 +0300

    hw_1

---
+++ src/model/Female.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Female extends Person {
+
+
+    public Female(String firstname, String lastname, String dateBirth, Family family) {
+        super(firstname, lastname, dateBirth, family);
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println(this.firstname + " " + this.lastname + " " + this.dateBirth);
+    }
+}
